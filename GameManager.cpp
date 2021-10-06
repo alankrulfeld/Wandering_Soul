003267b59fa9cf137f504d688b0ee327d2ba1e76
Francisco Emilio Cavenaghi <franchucave@hotmail.com>
2021-10-06 12:31:45 -0300

    Added class: GameManager.

---
+++ GameManager.cpp
@@ -0,0 +1,92 @@
+#include "GameManager.h"
+#include <iostream>
+
+GameManager::GameManager() {
+	isPlaying = true;
+}
+
+GameManager::~GameManager() {
+}
+
+STATES GameManager::GetState() {
+	return currentState;
+}
+
+void GameManager::SetState(STATES& states) {
+	currentState = states;
+}
+
+bool GameManager::ChangeState(STATES states) {
+	if (currentState == states) {
+		return false;
+	}
+	else {
+		SetState(states);
+		return true;
+	}
+}
+
+void GameManager::StartGameManager() {
+	UpdateState(GetState());
+}
+
+bool GameManager::QuitGame() {
+	return isPlaying;
+}
+
+void GameManager::UpdateState(STATES states) {
+	switch (GetState()) {
+	case STATES::MENU:
+		if (isThisStateStarting) {
+			menu = new Menu();
+			isThisStateStarting = false;
+		}
+		if (!(menu->GetInited())) {
+			menu->Init();
+		}
+		menu->Input();
+		menu->Update();
+		menu->Draw();
+		if (menu->ExitMenuGoToGame()) {
+			currentState = STATES::GAME;
+			menu->DeInit();
+			delete menu;
+			isThisStateStarting = true;
+		}
+		else if (menu->ExitGame()) {
+			currentState = STATES::EXIT;
+			menu->DeInit();
+			delete menu;
+			isThisStateStarting = true;
+		}
+		break;
+	case STATES::GAME:
+		if (isThisStateStarting) {
+			game = new Game();
+			isThisStateStarting = false;
+		}
+		if (!(game->GetInited())) {
+			game->InitGame();
+		}
+		game->UpdateDrawFrame();
+		if (game->GoToMenu()) {
+			currentState = STATES::MENU;
+			game->DeInitGame();
+			delete game;
+			isThisStateStarting = true;
+		}
+		else if (game->GoToGame()) {
+			currentState = STATES::GAME;
+			game->DeInitGame();
+			delete game;
+			isThisStateStarting = true;
+		}
+		break;
+	case STATES::EXIT:
+		isPlaying = false;
+		break;
+	default:
+		break;
+	}
+}
+
---
+++ GameManager.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "Menu.h"
+#include "Game.h"
+
+enum class STATES { MENU, EXIT, GAME, SHOP };
+
+class GameManager {
+private:
+	STATES currentState = STATES::MENU;
+	bool isThisStateStarting = true;
+	bool isPlaying;
+	Menu* menu;
+	Game* game;
+public:
+	GameManager();
+	~GameManager();
+	STATES GetState();
+	void SetState(STATES& states);
+	void UpdateState(STATES states);
+	bool ChangeState(STATES states);
+	void StartGameManager();
+	bool QuitGame();
+};
+
+
