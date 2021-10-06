afcadcccb76cef844c37bd0adf9e0f8713eab659
Francisco Emilio Cavenaghi <franchucave@hotmail.com>
2021-10-06 12:36:04 -0300

    Added class: Game.

---
+++ Game.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "Textures.h"
+
+class Game {
+private:
+	bool gameInited;
+	bool goToMenu;
+	bool replay;
+	bool isPaused;
+	bool goToGame;
+
+public:
+	const int screenWidth = 1366;
+	const int screenHeight = 768;
+	float currentScreenWidth = GetScreenWidth();
+	float currentScreenHeight = GetScreenHeight();
+	float windowReSizeWidth = currentScreenWidth / screenWidth;
+	float windowReSizeHeight = currentScreenHeight / screenHeight;
+	Game();
+	~Game();
+	bool GetInited();
+	void InitGame();         // Initialize game
+	void InputGame();
+	void UpdateGame();       // Update game (one frame)
+	void DrawGame();         // Draw game (one frame)
+	void DeInitGame();       // Unload game
+	void UpdateDrawFrame();  // Update and Draw (one frame)
+	bool GoToMenu();
+	bool GoToGame();
+};
+
